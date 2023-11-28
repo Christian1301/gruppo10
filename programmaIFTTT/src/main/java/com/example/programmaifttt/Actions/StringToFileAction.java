77b4b40e4586f693b3dc21f9a49c8ce76afad9b2
Mamac <m.magliulo2@studenti.unisa.it>
2023-11-28 17:29:27 +0100

    StringToFileAction completed

--- programmaIFTTT/src/main/java/com/example/programmaifttt/Actions/StringToFileAction.java
+++ programmaIFTTT/src/main/java/com/example/programmaifttt/Actions/StringToFileAction.java
@@ -2,23 +2,28 @@ package com.example.programmaifttt.Actions;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
-public class StringToFileAction {
- String message;
- File file;
-    public StringToFileAction(String message, File file){
-    this.message=message;
-    this.file=file;
+public class StringToFileAction extends Action{
+    public static final String type = "String to File";
+    String message;
+    File file;
+    public StringToFileAction(String name, String message, File file){
+        super(name,type,"File: " + file.getName() + "/message: " + message);
+        this.message=message;
+        this.file=file;
     }
 
-public void   FileWriter(String message, File file){
-    try {
-        FileWriter myWriter = new FileWriter(file);
-        myWriter.write(message);
-        myWriter.close();
-    } catch (IOException e) {
-        System.out.println("An error occurred.");
-        e.printStackTrace();
-    }
-}
 
+    @Override
+    public boolean execute() {
+        try {
+            FileWriter myWriter = new FileWriter(file);
+            myWriter.write(message);
+            myWriter.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
